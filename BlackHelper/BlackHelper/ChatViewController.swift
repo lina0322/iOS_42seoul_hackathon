75457080a6f7dadf2c06e085ab6e51e10b0dc5e7
주 <jjoo@juui-MacBookPro.local>
2020-12-17 21:00:30 +0900

    Add : IRC

--- BlackHelper/BlackHelper/ChatViewController.swift
+++ BlackHelper/BlackHelper/ChatViewController.swift
@@ -6,24 +6,26 @@
 //
 
 import UIKit
-
 class ChatViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let username = CadetData.me!.username
+        let user = IRCUser(username: username, realName: username, nick: username)
+        let server = IRCServer.connect("192.168.0.5", port: 6667, user: user)
+        
+        let channel = server.join("Libft")
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
